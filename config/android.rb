78ce112447454f4be7f7fa3b56176efadad886d2
Greg Molnar <molnargerg@gmail.com>
2017-12-14 19:59:35 +0100

    vendor android design

--- config/android.rb
+++ config/android.rb
@@ -9,4 +9,6 @@ end
 Motion::Project::App.setup do |app|
   # Use `rake android:config' to see complete project settings.
   app.name = 'Hello'
+  app.api_version = '25'
+  app.vendor_project :jar => "vendor/design-25.4.0/design-25.4.0.jar", :resources => "vendor/design-25.4.0/res", :manifest => "vendor/design-25.4.0//AndroidManifest.xml"
 end
